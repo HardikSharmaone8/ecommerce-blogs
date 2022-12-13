8f12475f9552182a576342103fc9c3c7065af62c
HardikSharmaone8 <hardiksharma30018@gmail.com>
2022-12-13 21:51:01 +0530

    deploy to vercel

--- Hkart/settings.py
+++ Hkart/settings.py
@@ -122,10 +122,6 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =os.path.join(BASE_DIR,'static')
-
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
-
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
@@ -141,3 +137,6 @@ django_heroku.settings(locals())
 MESSAGE_TAGS = {
     messages.SUCCESS : "greenyellow"
 }
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
