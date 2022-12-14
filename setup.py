f683341e37480a0ffb8eaaca99e2f22069ef4d7a
HardikSharmaone8 <hardiksharma30018@gmail.com>
2022-12-14 18:02:12 +0530

    deploy to vercel

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="Hkart",
+version="0.1",
+description="This is ecommerce and blog project created by Hardik Sharma",
+Long_description="This is ecommerce and blog project created by Hardik",
+author="Hardik Sharma",
+packages=['Hkart'],
+install_requires=[])
